335adf75c731f330d15798da8253e21825d81112
shannonmaynard <55810743+shannonmaynard@users.noreply.github.com>
2021-02-03 16:28:28 -0800

    .

--- my-app/pages/BuddingWriters/index.js
+++ my-app/pages/BuddingWriters/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 import Categories from "../comps/Categories";
 import Slideshow2 from "../comps/Slideshow2";
