25a47371a1373413785cfacf0f39dd1a3870aa34
shannonmaynard <55810743+shannonmaynard@users.noreply.github.com>
2021-02-10 17:52:58 -0800

    Add files via upload

---
+++ comps/Template/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const Cont = styled.div`
+ 
+ 
+`;
+
+
+/*
+  @media only screen and (max-width: 700px) {
+
+  }
+*/
+
+const Template = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="Menu">
+      
+    </div>
+  );
+};
+
+export default Template;
