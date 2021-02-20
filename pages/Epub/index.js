5c165968c6cc5396da9e7e1b44de474d7651dd57
shannonmaynard <55810743+shannonmaynard@users.noreply.github.com>
2021-02-20 00:50:48 -0800

    stopping work on epub for tonight

--- pages/Epub/index.js
+++ pages/Epub/index.js
@@ -45,42 +45,10 @@ const P = styled.div`
   align-items: flex-start;
 `;
 
-const PInfo = styled.div`
-  font-weight: 400;
-  width: 100%;
-  padding: 100px 0px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: white;
-`;
-
-const Img = styled.img`
-  width: 100%;
-  max-width: 900px;
-  height: auto;
-  border: black solid 15px;
-`;
-const ContLinks = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
 const P2 = styled.p`
   margin-top: 0px;
 `;
 const Li = styled.li``;
-const H3 = styled.h3`
-  display: flex;
-  width: 100%;
-  justify-content: flex-start;
-  align-items: left;
-  text-align: left;
-  @media only screen and (max-width: 700px) {
-    justify-content: flex-start;
-    align-items: flex-start;
-  }
-`;
 
 const Epub = () => {
   return (
@@ -104,28 +72,25 @@ const Epub = () => {
           <br></br>
           <br></br>
           <div className="contLinks">
-            <a
+            <Btn
               href="https://indd.adobe.com/view/28d31d63-53c3-4a49-ab69-1aa7379a1ca7"
               target="_blank"
             >
               <a className="btn">View Epub Online</a>
-            </a>
+            </Btn>
             <br></br> <br></br> <br></br>
-            <a
+            <Btn2
               href="https://drive.google.com/file/d/1xktBsIBLpbr7hmPH9x1hUEUYLH8DUuyc/view?usp=sharing"
               target="_blank"
             >
-              <a className="btn2">Download Epub Link</a>
-            </a>
+              <a className="btn">Download Epub Link</a>
+            </Btn2>
           </div>
         </div>
-        <br></br>
-        <br></br> <br></br>
-        <br></br> <br></br>
-        <br></br>
+
         <div className="info1">
-          <P>
-            <H3>Project Task/Requirements</H3>
+          <div className="epubTasks">
+            <h3>Project Task/Requirements</h3>
             <br></br>
             <br></br>
             <P2>
@@ -138,11 +103,11 @@ const Epub = () => {
               content, and states.
               <br></br>
               <br></br>
-              <b>Description </b>
+              <b>What Was Asked For the Project</b>
               <br></br>
               <br></br>
-              You will design and implement an e-book/e-publication with
-              interactive features, such as (but not limited to):
+              To design and implement an e-book/e-publication with interactive
+              features, such as (but not limited to):
             </P2>
             <Li>audio and videos</Li>
             <Li>buttons and navigation</Li>
@@ -156,80 +121,90 @@ const Epub = () => {
             <Li>scrollable areas</Li>
             <Li>Interactive branching</Li>
             <Li>and more!</Li>
-          </P>
+            <br></br>
+            <br></br>
+            <b>Plot</b>
+            <p>
+              A childrens game/story about a hamster named Peanut that longs for
+              freedom, so she escapes. The players job is to protect her when
+              she reaches the outside world and get her home safely.
+            </p>
+          </div>
         </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className="info2">
-          <P>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
 
+        <div className="info2">
+          <P2>
+            <h3>Ideation</h3>
+            <br></br> <br></br>
+            <b>Ideation: The Story of the Original Peanut</b>
+            <br></br> <br></br>
+            My EPUB project was inspired by a true story. At the beginning of
+            the year in the month of september, my younger sister got herself a
+            pet hamster. On the first day she brought her hamster, that she
+            named Peanut, home Peanut almost escaped. She'd opened the the cage
+            for just a second, and peanut took the chance to escape. She ran
+            extremely quickly, and before we knew it, she'd fit herself into a
+            small hole that led inside of the fireplace. We all started
+            immediately freaking out.
             <br></br>
-            <P2>
-              <h3>Ideation</h3>
-              <br></br> <br></br>
-              <b>Ideation: The Story of the Original Peanut</b>
-              <br></br> <br></br>
-              My EPUB project was inspired by a true story. At the beginning of
-              the year in the month of september, my younger sister got herself
-              a pet hamster. On the first day she brought her hamster, that she
-              named Peanut, home Peanut almost escaped. She'd opened the the
-              cage for just a second, and peanut took the chance to escape. She
-              ran extremely quickly, and before we knew it, she'd fit herself
-              into a small hole that led inside of the fireplace. We all started
-              immediately freaking out.
-              <br></br>
-              <br></br>
-              After a bit of panic, we pulled off a metal piece that was closing
-              off the fire place and after taking a look inside and saw her at
-              the very back, next to a different hole that led who knew where.
-              She seemed very scared, so going in, we knew we had one chance to
-              grab her. So we led her closer to us with a trail of food, then
-              when the moment was right, we grabbed her!
-              <br></br>
-              <br></br>
-              <b>Bringing the Scene to Life: a Children's Book</b> <br></br>
-              <br></br>
-              That scene stuck with me, and when I was thinking about ideas for
-              this project, I thought about this scene. This led to me fleshing
-              out idea of, what if Peanut actually did escape? If she hadn't
-              died, what would be her life outside? As I was aiming to do a
-              children's book, these wild imaginations seemed like an
-              interesting idea. So, I turned these thoughts into a concrete
-              idea. I knew it would work well, as many children have hamsters as
-              their first pet. If they were to play my game, they could
-              sympathize and have fun while helping Peanut in her outside
-              adventures.
-            </P2>
-          </P>
+            <br></br>
+            After a bit of panic, we pulled off a metal piece that was closing
+            off the fire place and after taking a look inside and saw her at the
+            very back, next to a different hole that led who knew where. She
+            seemed very scared, so going in, we knew we had one chance to grab
+            her. So we led her closer to us with a trail of food, then when the
+            moment was right, we grabbed her!
+            <br></br>
+            <br></br>
+            <b>Bringing the Scene to Life: a Children's Book</b> <br></br>
+            <br></br>
+            That scene stuck with me, and when I was thinking about ideas for
+            this project, I thought about this scene. This led to me fleshing
+            out idea of, what if Peanut actually did escape? If she hadn't died,
+            what would be her life outside? As I was aiming to do a children's
+            book, these wild imaginations seemed like an interesting idea. So, I
+            turned these thoughts into a concrete idea. I knew it would work
+            well, as many children have hamsters as their first pet. If they
+            were to play my game, they could sympathize and have fun while
+            helping Peanut in her outside adventures.
+          </P2>
         </div>
         <div className="chara">
           <div className="epubRowChara">
-            <h3>The Characters:</h3>
-            <h5> Hero and Villain</h5>
-            <img className="charaImg2" src="/charas.png" />
-            <h4>The Cats</h4>
-            <img className="charaImg" src="/cats.jpeg" />
-            <p>
-              Based on the real life cats of my older sister. Their unamed in
-              the book but in real life go by Boo and Harley, two strays that
-              were adopted when they were already grown.
-            </p>
+            <div className="charaCont">
+              <h3>The Characters:</h3>
+              <br></br>
+              <h5> Hero and Villain</h5>
+              <br></br>
+              <img className="charaImg" src="/charas.png" />
+            </div>
+            <br></br> <br></br> <br></br>
+            <div className="charaCont1">
+              <h4>The Cats</h4>
+              <div className="charaRow">
+                <img className="charaImg2" src="/cats.jpeg" />
+
+                <p className="p">
+                  Based on the real life cats of my older sister. They're unamed
+                  in the book but in real life go by Boo and Harley; two strays
+                  with a scarred past that were adopted later in their life.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="epubRowChara">
+          <br></br> <br></br> <br></br> <br></br>
+          <div className="epubRowChara1">
             <h4>Hamster Peanut</h4>
-            <img className="charaImg" src="/peanut.png" />
-            <p>
-              Based on the real life hamster Peanut, bought from a local small
-              pet store at a young age by my younger sister.
-            </p>
+            <div className="charaRow">
+              <img className="charaImg2" src="/peanut.png" />
+              <div className="p">
+            
+                <p>
+                  Based on the real life hamster Peanut, bought from a local
+                  small pet store at a young age by my younger sister.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
